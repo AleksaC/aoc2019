f473f1a4bc82167bc16393778f3e865d23005447
AleksaC <aleksacukovic1@gmail.com>
2019-12-08 21:13:00 +0100

    Refactor solution for day07 to be slightly less ugly

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,93 +1,4 @@
-fn compute(intcode: &mut Vec<i32>, input_1: i32, input_2: i32, mut first: bool) -> i32 {
-    let n = intcode.len();
-    let mut ip = 0;
-
-    while ip <= n {
-        let instruction = intcode[ip];
-
-        let opcode = instruction % 100;
-
-        if opcode  < 3 {
-            let k = intcode[ip +1] as usize;
-            let l = intcode[ip +2] as usize;
-            let j = intcode[ip +3] as usize;
-
-            let p = if (instruction % 1000) / 100 == 0 { intcode[k] } else { k as i32 };
-            let q = if (instruction % 10_000) / 1000 == 0 { intcode[l] } else { l as i32 };
-
-            intcode[j] = if opcode == 1 { p + q } else { p * q };
-
-            ip += 4;
-        } else if opcode < 5 {
-            let j = intcode[ip +1] as usize;
-
-            if opcode == 3 {
-                if first {
-                    intcode[j] = input_1;
-                    first = false;
-                } else {
-                    intcode[j] = input_2;
-                }
-            } else {
-                return if (instruction % 1000) / 100 == 0 { intcode[j] } else { j as i32 };
-            }
-
-            ip += 2;
-        } else if opcode < 7 {
-            let mut k = intcode[ip +1] as usize;
-            let mut l = intcode[ip +2] as usize;
-
-            if (instruction % 1000) / 100 == 0 {
-                k = intcode[k] as usize;
-            }
-            if (instruction % 10_000) / 1000 == 0 {
-                l = intcode[l] as usize;
-            }
-
-            let z = k == 0;
-
-            if opcode == 5 && !z || opcode == 6 && z {
-                ip = l;
-            } else {
-                ip += 3;
-            }
-        } else if opcode < 9 {
-            let mut k = intcode[ip +1] as usize;
-            let mut l = intcode[ip +2] as usize;
-            let j = intcode[ip +3] as usize;
-
-            if (instruction % 1000) / 100 == 0 {
-                k = intcode[k] as usize;
-            }
-            if (instruction % 10_000) / 1000 == 0 {
-                l = intcode[l] as usize;
-            }
-
-            if opcode == 7 && k < l || opcode == 8 && k == l {
-                intcode[j] = 1;
-            } else {
-                intcode[j] = 0;
-            }
-
-            ip += 4;
-        } else {
-            break;
-        }
-    }
-    return -1;
-}
-
 fn compute_signal(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
-    let mut prev = 0;
-
-    for i in 0..5 {
-        prev = compute(&mut intcode.clone(), phase_params[i], prev, true);
-    }
-
-    return prev;
-}
-
-fn compute_with_loop(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
     let mut codes = vec![intcode.clone(); 5];
     let mut ips = vec![0; 5];
 
@@ -106,10 +17,10 @@ fn compute_with_loop(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
                 let instruction = intcode[ip];
                 let opcode = instruction % 100;
 
-                if opcode  < 3 {
-                    let k = intcode[ip +1] as usize;
-                    let l = intcode[ip +2] as usize;
-                    let j = intcode[ip +3] as usize;
+                if opcode < 3 {
+                    let k = intcode[ip + 1] as usize;
+                    let l = intcode[ip + 2] as usize;
+                    let j = intcode[ip + 3] as usize;
 
                     let p = if (instruction % 1000) / 100 == 0 { intcode[k] } else { k as i32 };
                     let q = if (instruction % 10_000) / 1000 == 0 { intcode[l] } else { l as i32 };
@@ -118,7 +29,7 @@ fn compute_with_loop(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
 
                     ip += 4;
                 } else if opcode < 5 {
-                    let j = intcode[ip +1] as usize;
+                    let j = intcode[ip + 1] as usize;
 
                     if opcode == 3 {
                         if inner_first {
@@ -135,8 +46,8 @@ fn compute_with_loop(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
 
                     ip += 2;
                 } else if opcode < 7 {
-                    let mut k = intcode[ip +1] as usize;
-                    let mut l = intcode[ip +2] as usize;
+                    let mut k = intcode[ip + 1] as usize;
+                    let mut l = intcode[ip + 2] as usize;
 
                     if (instruction % 1000) / 100 == 0 {
                         k = intcode[k] as usize;
@@ -153,9 +64,9 @@ fn compute_with_loop(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
                         ip += 3;
                     }
                 } else if opcode < 9 {
-                    let mut k = intcode[ip +1] as usize;
-                    let mut l = intcode[ip +2] as usize;
-                    let j = intcode[ip +3] as usize;
+                    let mut k = intcode[ip + 1] as usize;
+                    let mut l = intcode[ip + 2] as usize;
+                    let j = intcode[ip + 3] as usize;
 
                     if (instruction % 1000) / 100 == 0 {
                         k = intcode[k] as usize;
@@ -184,6 +95,31 @@ fn compute_with_loop(intcode: &Vec<i32>, phase_params: Vec<i32>) -> i32 {
     return out;
 }
 
+fn compute_max(initial_memory: &Vec<i32>, l: i32, u: i32) -> i32 {
+    let mut max = 0;
+
+    for i in l..u {
+        for j in l..u {
+            if j == i { continue; }
+            for k in l..u {
+                if k == i || k == j { continue; }
+                for l in l..u {
+                    if l == i || l == j || l == k { continue; }
+                    for m in l..u {
+                        if m == i || m == j || m == k || m == l { continue; }
+                        let tot = compute_signal(initial_memory, vec![i, j, k, l, m]);
+                        if tot > max {
+                            max = tot;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return max;
+}
+
 fn main() {
     let initial_memory: Vec<i32> = include_str!("../input.txt")
         .trim()
@@ -191,49 +127,6 @@ fn main() {
         .map(|x| x.parse().unwrap())
         .collect();
 
-    let mut max = 0;
-
-    for i in 0..5 {
-        for j in 0..5 {
-            if j == i { continue; }
-            for k in 0..5 {
-                if k == i || k == j { continue; }
-                for l in 0..5 {
-                    if l == i || l == j || l == k { continue; }
-                    for m in 0..5 {
-                        if m == i || m == j || m == k || m == l { continue; }
-                        let tot = compute_signal(&initial_memory, vec![i, j, k, l, m]);
-                        if tot > max {
-                            max = tot;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    println!("Part one: {}", max);
-
-    max = 0;
-
-    for i in 5..10 {
-        for j in 5..10 {
-            if j == i { continue; }
-            for k in 5..10 {
-                if k == i || k == j { continue; }
-                for l in 5..10 {
-                    if l == i || l == j || l == k { continue; }
-                    for m in 5..10 {
-                        if m == i || m == j || m == k || m == l { continue; }
-                        let tot = compute_with_loop(&initial_memory, vec![i, j, k, l, m]);
-                        if tot > max {
-                            max = tot;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    println!("Part two: {}", max);
+    println!("Part one: {}", compute_max(&initial_memory, 0, 5));
+    println!("Part two: {}", compute_max(&initial_memory, 5, 10));
 }
